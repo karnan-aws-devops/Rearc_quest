17e602858bc6d5883b92b8a66f086add5591ff74
Karnan P <karnanpandian12@gmail.com>
2025-07-26 01:21:00 +0530

    commit with security_group modules

---
+++ terraform/modules/security_group/main.tf
@@ -0,0 +1,32 @@
+resource "aws_security_group" "quest_sg" {
+  name        = var.sg_name
+  description = "Allow SSH and HTTP"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "https"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "http"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
